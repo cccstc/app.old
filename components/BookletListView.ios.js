a662b5e72c30742c61262631a1a48929a97cd174
K.K. POON <noopkk@gmail.com>
2016-10-10 00:21:23 +0800

    add TabView, Booklet Listing

---
+++ components/BookletListView.ios.js
@@ -0,0 +1,105 @@
+import React, { Component } from 'react';
+import {
+  ListView,
+  RefreshControl,
+  View,
+  WebView,
+  TouchableHighlight,
+  Text,
+  StyleSheet,
+} from 'react-native';
+import { BookletObservable } from "../lib/api";
+
+const ds = new ListView.DataSource({
+  rowHasChanged: (r1, r2) => r1.date !== r2.date
+});
+
+const BookletListItem = ({booklet, navigator}) => (
+  <TouchableHighlight onPress={() => {navigator.push({
+      title: booklet.date,
+      component: WebView,
+      passProps: {
+        source: {uri: booklet.booklet},
+      }
+    })}}>
+    <View>
+      <View style={styles.row}>
+        <Text style={styles.title}>{booklet.date}</Text>
+        <Text style={styles.text}>第{booklet.week}週</Text>
+      </View>
+    </View>
+  </TouchableHighlight>
+);
+
+const ListSeparator = (sectionID, rowID, adjacentRowHighlighted) => (
+  <View
+    key={`${sectionID}-${rowID}`}
+    style={{
+      height: adjacentRowHighlighted ? 4 : 1,
+      backgroundColor: adjacentRowHighlighted ? '#3B5998' : '#CCCCCC',
+    }}
+  />
+);
+
+export class BookletListView extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      booklets: [],
+      refreshing: false,
+    }
+  }
+
+  componentWillMount() {
+    this.fetchBookletList();
+  }
+
+  render() {
+    //console.warn(this.props.navigator);
+    const { navigator } = this.props;
+    const { booklets, refreshing } = this.state;
+    return (
+      <ListView
+        dataSource={ds.cloneWithRows(booklets)}
+        renderRow={(booklet) => BookletListItem({booklet, navigator})}
+        renderSeparator={ListSeparator}
+        enableEmptySections={true}
+        refreshControl={
+          <RefreshControl
+            refreshing={refreshing}
+            onRefresh={() => this.fetchBookletList()}
+          />
+        }
+      />
+    );
+  }
+
+  fetchBookletList() {
+    this.setState({refreshing: true});
+    BookletObservable()
+      .toArray()
+      .subscribe(
+        (booklets) => this.setState({booklets: booklets}),
+        (err) => console.error(err),
+        () => this.setState({refreshing: false}),
+      );
+  }
+}
+
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: 'column',
+    justifyContent: 'center',
+    padding: 10,
+    paddingLeft: 30,
+  },
+  title: {
+    fontSize: 28,
+    flex: 1,
+  },
+  text: {
+    fontSize: 16,
+    marginTop: 5,
+    flex: 1,
+  },
+});
--- index.ios.js
+++ index.ios.js
@@ -8,46 +8,44 @@ import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
+  TabBarIOS,
+  View,
+  Modal,
   Text,
-  View
+  TouchableHighlight,
 } from 'react-native';
 
+import { BookletTab } from "./booklet.ios";
+import { RecordTab } from "./record.ios";
+
 class ChurchApp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      records: [],
+      currentTab: 1,
+      hasProblem: null,
+    };
+  }
+
   render() {
+    const { records, currentTab } = this.state;
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <TabBarIOS>
+        <BookletTab
+          selected={currentTab === 1}
+          onPress={() => this.setState({currentTab: 1})}
+        />
+        <RecordTab
+          records={records}
+          selected={currentTab === 2}
+          onPress={() => this.setState({currentTab: 2})}
+        />
+      </TabBarIOS>
     );
   }
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+const styles = StyleSheet.create({});
 
 AppRegistry.registerComponent('ChurchApp', () => ChurchApp);
